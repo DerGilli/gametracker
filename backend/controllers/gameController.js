81e2598993455eb4f11c3cc1f1b2423087a55997
Pascal <p.gilmer@gmx.de>
2023-01-10 20:47:18 +0100

    authentication

--- backend/controllers/gameController.js
+++ backend/controllers/gameController.js
@@ -2,15 +2,17 @@ const asyncHandler = require('express-async-handler');
 const gameModel = require('../models/gameModel');
 
 const Game = require('../models/gameModel')
+const User = require('../models/userModel')
 
 // @desc    Get games
 // @route   GET /api/games
 // @access  Private
 const getGames = asyncHandler(async (req, res) => {
-    const games = await Game.find();
+    const games = await Game.find({ user: req.user.id });
 
     res.json(games);
 })
+
 // @desc    Set game
 // @route   POST /api/games
 // @access  Private
@@ -21,11 +23,13 @@ const setGame = asyncHandler(async (req, res) => {
     }
 
     const game = await Game.create({
-        name: req.body.name
+        name: req.body.name,
+        user: req.user.id,
     })
 
     res.json(game);
 })
+
 // @desc    Update game
 // @route   GET /api/game/:id
 // @access  Private
@@ -37,12 +41,25 @@ const updateGame = asyncHandler(async (req, res) => {
         throw new Error('Game not found');
     }
 
+    const user = await User.findById(req.user.id);
+
+    if (!user) {
+        res.status(401);
+        throw new Error('User not found')
+    }
+
+    if (game.user.toString() !== user.id) {
+        res.status(401);
+        throw new Error('Not allowed')
+    }
+
     const updatedGame = await Game.findByIdAndUpdate(req.params.id, req.body, {
         new: true
     });
 
     res.json(updatedGame);
 })
+
 // @desc    Delete game
 // @route   DELETE /api/games/:id
 // @access  Private
@@ -52,6 +69,18 @@ const deleteGame = asyncHandler(async (req, res) => {
     if (!game) {
         res.status(400);
         throw new Error('Game not found');
+    }
+
+    const user = await User.findById(req.user.id);
+
+    if (!user) {
+        res.status(401);
+        throw new Error('User not found')
+    }
+
+    if (game.user.toString() !== user.id) {
+        res.status(401);
+        throw new Error('User not authorized')
     }
 
     await game.remove();
--- backend/routes/gameRoutes.js
+++ backend/routes/gameRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { getGames, setGame, updateGame, deleteGame } = require('../controllers/gameController')
+const { protect } = require('../middleware/authMiddleware')
 
-router.route('/').get(getGames).post(setGame);
-router.route('/:id').put(updateGame).delete(deleteGame);
+router.route('/').get(protect, getGames).post(protect, setGame);
+router.route('/:id').put(protect, updateGame).delete(protect, deleteGame);
 
 module.exports = router;
